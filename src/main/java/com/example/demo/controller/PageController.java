1070cc99ac5cad34eadea9afca1ab928c9d9a537
M Kamal <muh.kmal@gmail.com>
2018-02-24 20:00:12 +0700

    Push perkalian update

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -4,23 +4,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
 
 @Controller
-public class PageController
-{
-	 @RequestMapping("/hello")
-	 public String index ()
-	 {
-		 return "hello";
-	 }
-	 
-	 @RequestMapping("/greeting")
-	// public String greeting (@RequestParam(value = "name", required = false) String
-	//		 name, Model model)
-	 public String greeting (@RequestParam(value = "name", required = false,
-	 defaultValue = "dunia") String name, Model model)
-	 {
-	  model.addAttribute ("name​", name);
-	  return "greeting";
-	 }
+public class PageController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+	
+	//@RequestMapping("/greeting")
+	//public String greeting (@RequestParam(value = "name", required = false, defaultValue = "dunia") String name, Model model)
+//	{
+	// model.addAttribute ("name​", name);
+	// return "greeting";
+//	}
+	
+	@RequestMapping(value = {"/greeting", "greeting/{name}"})
+	public String greetingPath(@PathVariable Optional<String> name, Model model) {
+		
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());
+		} 
+		else {
+			model.addAttribute("name", "apap");
+		}
+		
+	return "greeting";
+	
+	}
+	
+	@RequestMapping("/perkalian")
+	public String perkalian(@RequestParam(value = "a", defaultValue = "0") String a,
+			@RequestParam(value = "b", defaultValue = "0") String b, Model model) {
+		
+		model.addAttribute("a​", "100");
+		model.addAttribute("b​", b);
+		
+	return "perkalian";
+	
+	}
+	
 }
